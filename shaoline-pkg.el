d91aaefe4ba33645025e4011a6b7c03cbd11b78f
Peter Kosov <11111000000@email.com>
2025-07-04 22:47:59 +0800

    define-package

--- shaoline-pkg.el
+++ shaoline-pkg.el
@@ -1,2 +1,2 @@
 ;;; Generated package description from shaoline.el  -*- no-byte-compile: t -*-
-(define-package "shaoline" "0.1" "No description available." 'nil :kind vc)
+(define-package "shaoline" "1.0" "No description available." '((emacs "27.1")) :kind vc :commit "fe51de2db6584f5446eab3fd3359ff37bf258a21")
